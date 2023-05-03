f0f7c421be78273e078531854ebdc6d7a953d51f
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-03 23:18:53 +0800

    Profile macro in exlib

--- experiment/exlib/include/exlib.h
+++ experiment/exlib/include/exlib.h
@@ -4,6 +4,27 @@
 # include "libft.h"
 # include <sys/time.h>
 
+# define PROFILE_RET(prefix, expression)	({ \
+	struct timeval	__start__; \
+	struct timeval	__end__; \
+\
+	gettimeofday(&__start__, NULL); \
+	__typeof__(expression)	result = expression; \
+	gettimeofday(&__end__, NULL); \
+	printf("%s: %.3f ms\n", prefix, elapsed_time(__start__, __end__)); \
+	result; \
+})
+
+# define PROFILE(prefix, expression)	({ \
+	struct timeval	__start__; \
+	struct timeval	__end__; \
+\
+	gettimeofday(&__start__, NULL); \
+	expression; \
+	gettimeofday(&__end__, NULL); \
+	printf("%s: %.3f ms\n", prefix, elapsed_time(__start__, __end__)); \
+})
+
 double	elapsed_time(struct timeval start, struct timeval end);
 void	*time_debug(const char *prefix, void *(*ft)(void *), void *arg);
 
