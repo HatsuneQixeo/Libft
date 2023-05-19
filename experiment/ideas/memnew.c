b5ebd42f8d250ae7bf7ce2133d38e57d0337aa78
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-19 22:17:55 +0800

    more experiment

--- experiment/ideas/memnew.c
+++ experiment/ideas/memnew.c
@@ -4,6 +4,10 @@
  * @brief Makes a duplicate of the given data of given size in heap
  * (Shallow Copy)
  * 
+ * This function could be dangerous if assumed to always return a valid address,
+ * the allocated memory inside data should be freed if this malloc fails.
+ * 
+ * Which, defeat the purpose of this function entirely
  * @param data 
  * @param size 
  * @return void* 
