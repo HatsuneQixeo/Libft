39bb99424c159a38c57f7442b3518e7aa35371a1
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-20 17:55:38 +0800

    header and norm

--- srcs/is/ft_memisempty.c
+++ srcs/is/ft_memisempty.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memisempty.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hqixeo <hqixeo@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/20 17:53:48 by hqixeo            #+#    #+#             */
+/*   Updated: 2023/05/20 17:53:48 by hqixeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdlib.h>
 
 /* Maybe someday? */
 int	ft_memisempty(const void *ptr, const size_t bytes)
 {
-	const char	*begin = ptr;
-	const char	*end = begin + bytes;
+	const char	*it = ptr;
+	const char	*end = it + bytes;
 
-	while (begin != end && *begin == 0b0)
-		begin++;
-	return (begin == end);
+	while (it != end && *it == 0b0)
+		it++;
+	return (it == end);
 }
