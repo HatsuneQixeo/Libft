fd3d107281cc284193fe06114da5c4abae0562fc
Hatsune Qixeo <qiyong831@gmail.com>
2023-05-23 03:27:23 +0800

    Smarter atol/atoi I guess

--- include/libto.h
+++ include/libto.h
@@ -22,18 +22,25 @@
 /* To */
 t_list	*ft_aatolst(void **aa, t_ftmap ft_map, t_ftdel del);
 t_list	*ft_aatolst_clear(void **aa);
-int		ft_atoi(const char *str);
-long	ft_atol(const char *str);
+
+int		ft_atoi(const char *str, int *ret);
+int		ft_atol(const char *str, long *ret);
+int		ft_atol_range(const char *str, long *ret, long min, long max);
+
 char	*ft_itoa(int n);
 char	*ft_lltoa(long long n);
+
 char	*ft_lsttostr(t_list *lst);
 char	*ft_lsttostr_clear(t_list **lst);
 char	*ft_lsttostr_delimiter(t_list *lst, const char *delimiter);
 char	*ft_lsttostr_delimiter_clear(t_list **lst, const char *delimiter);
+
 void	**ft_lsttoaa(const t_list *lst, t_ftmap ft_map, t_ftdel del);
 void	**ft_lsttoaa_clear(t_list **lst);
+
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
 char	*ft_utoa_base(size_t n, const char *base);
 int		ft_validbase(const char *base);
 
--- srcs/to/ft_atoi.c
+++ srcs/to/ft_atoi.c
@@ -11,10 +11,10 @@
 /* ************************************************************************** */
 #include "libto.h"
 
-int	ft_atoi(const char *str)
+int	ft_atoi(const char *str, int *ret)
 {
-	int	sign;
 	int	nbr;
+	int	sign;
 
 	while (ft_isspace(*str))
 		str++;
@@ -24,11 +24,16 @@ int	ft_atoi(const char *str)
 			sign = -1;
 	nbr = 0;
 	while (ft_isdigit(*str))
+	{
 		nbr = (nbr * 10) + (*str++ - '0');
-	return (nbr * sign);
+		if (nbr * 10 < nbr)
+			return (-1);
+	}
+	*ret = nbr * sign;
+	return (0);
 }
 
-long	ft_atol(const char *str)
+int	ft_atol(const char *str, long *ret)
 {
 	long	nbr;
 	int		sign;
@@ -41,6 +46,21 @@ long	ft_atol(const char *str)
 			sign = -1;
 	nbr = 0;
 	while (ft_isdigit(*str))
+	{
 		nbr = (nbr * 10) + (*str++ - '0');
-	return (nbr * sign);
+		if (nbr * 10 < nbr)
+			return (-1);
+	}
+	*ret = nbr * sign;
+	return (0);
+}
+
+int	ft_atol_range(const char *str, long *ret, const long min, const long max)
+{
+	long	value;
+
+	if (ft_atol(str, &value) == -1 || !(min <= value && value <= max))
+		return (-1);
+	*ret = value;
+	return (0);
 }
